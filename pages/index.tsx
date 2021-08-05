a1d9b0dd8b6c84e8960978dac12d544751c0ad17
Tksi <Tksi@users.noreply.github.com>
2021-08-05 11:20:47 +0900

    [del] unnecessary return

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,25 +4,21 @@ import { PageList, getPageList } from 'lib/notionAPI';
 
 type Props = { pageList: PageList };
 
-export const getStaticProps = async (): Promise<{ props: Props }> => {
-  return {
-    props: { pageList: await getPageList() },
-  };
-};
+export const getStaticProps = async (): Promise<{ props: Props }> => ({
+  props: { pageList: await getPageList() },
+});
 
 const index: VFC<Props> = ({ pageList }) => {
   return (
     <>
       <ul>
-        {pageList.map((v) => {
-          return (
-            <li key={v.id}>
-              <Link href={`/posts/${v.id}`}>
-                <a>{v.title}</a>
-              </Link>
-            </li>
-          );
-        })}
+        {pageList.map((v) => (
+          <li key={v.id}>
+            <Link href={`/posts/${v.id}`}>
+              <a>{v.title}</a>
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
   );
